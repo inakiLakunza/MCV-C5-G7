c11e97291878f92c57dce0eaeb2d3e3e2fad42ee
ggcr <ggcristian@icloud.com>
2024-04-21 19:07:14 +0200

    Add inference model where we perform fusion

--- W6/task_g/full_dataloader.py
+++ W6/task_g/full_dataloader.py
@@ -11,8 +11,6 @@ import librosa.display
 import noisereduce as nr
 import numpy as np
 import torchaudio
-import sys
-import time
 
 def parse_data(LABELS_PATH):
     data = []
@@ -20,16 +18,25 @@ def parse_data(LABELS_PATH):
         if label.startswith('.'): continue
         complete_path = os.path.join(LABELS_PATH, label)
         all_paths = sorted(os.listdir(complete_path))
-        for path in all_paths:
-            if path.split('.')[-1] == 'wav':
-                data.append({"audio_path": os.path.join(LABELS_PATH, label, path), "label": label})
+        for i in range(0, len(all_paths) - 1, 3):
+            dict_files = {}
+            for j in range(i, i+3):
+                if all_paths[j].split('.')[-1] == 'jpg':
+                    dict_files["image_path"] = os.path.join(LABELS_PATH, label, all_paths[j])
+                elif all_paths[j].split('.')[-1] == 'wav':
+                    dict_files["audio_path"] = os.path.join(LABELS_PATH, label, all_paths[j])
+                elif all_paths[j].split('.')[-1] == 'pkl':
+                    dict_files["text_path"] = os.path.join(LABELS_PATH, label, all_paths[j])
+            dict_files["label"] = label
+            dict_files["videoName"] = all_paths[j].split('.')[:-1]
+            data.append(dict_files)
     return data
 
 
     
 
 class Dataset():
-    def __init__(self, regime="train", aug=None):
+    def __init__(self, regime="train"):
 
         assert regime.lower() in ["train", "val", "test"], "Chosen regime is not correct"
 
@@ -44,43 +51,52 @@ class Dataset():
 
         self.data = parse_data(LABELS_PATH)
 
+        if regime == 'train':
+            self.data_train_transform = transforms.Compose([
+                v2.Resize(size=224),
+                v2.TrivialAugmentWide(),
+                # Turn the image into a torch.Tensor
+                v2.ToTensor(), # this also converts all pixel values from 0 to 255 to be between 0.0 and 1.0 
+                # resnet50 normalization
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+        else:
+            self.data_train_transform = transforms.Compose([
+                v2.Resize(size=224),
+                # Turn the image into a torch.Tensor
+                v2.ToTensor(), # this also converts all pixel values from 0 to 255 to be between 0.0 and 1.0 
+                # resnet50 normalization
+                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+
     
     def __len__(self):
         return len(self.data)
-        
+    
+    def read_image(self, img_path):
+        img = Image.open(img_path).convert('RGB')
+        img_tensor = self.data_train_transform(img)
+        return img_tensor
+    
+    def read_text(self, text_path):
+        with open(text_path, 'rb') as f:
+            txt = pickle.load(f)
+        return txt
+    
     def read_audio(self, wav_path):
-        # waveform, sample_rate = torchaudio.load(wav_path)
-        #data, sr = librosa.load(wav_path, sr=8000)
-        # start = time.time()
         data, sr = torchaudio.load(wav_path)
-        # end = time.time()
-        # print("torchaudio load: " + str(end-start))
         data = data.squeeze()
-
-        # start = time.time()
         data_noise_reduced = nr.reduce_noise(y=data, sr=sr, stationary=False)
-        # end = time.time()
-        # print("data_noise_reduced load: " + str(end-start))
 
-        # start = time.time()
         onsets = librosa.onset.onset_detect(
             y=data_noise_reduced, sr=sr, 
             units="time", hop_length=128, backtrack=False
         )        
-        # end = time.time()
-        # print("onsets load: " + str(end-start))
-
-
         duration = 15.302
         number_of_words = len(onsets)
         words_per_second = number_of_words / duration
 
-        # start = time.time()
         tempo = librosa.beat.tempo(y=data_noise_reduced, sr=sr, start_bpm=10)[0] 
-        # end = time.time()
-        # print("tempo: " + str(end-start))
-
-        # start = time.time()
         f0, _, _ = librosa.pyin(y=data_noise_reduced, sr=sr,
                                 fmin=10, fmax=450, frame_length=4096)
         
@@ -88,12 +104,7 @@ class Dataset():
         if np.isnan(f0).all(): 
             return_list = [0.0]*7
             return torch.tensor(return_list).to(torch.float)
-
-        # end = time.time()
-        # print("f0 pyin: " + str(end-start))
-        #timepoints = np.linspace(0, duration, num=len(f0), endpoint=False)
-
-        # start = time.time()
+        
         f0_values = [
             np.nanmean(f0),
             np.nanmedian(f0),
@@ -105,15 +116,9 @@ class Dataset():
         new_f0_values = []
         for value in f0_values:
             if value == np.isnan:
-                #print("nan found")
                 new_f0_values.append(0.0)
             else: 
                 new_f0_values.append(value)
-
-        # end = time.time()
-        # print("f0 values: " + str(end-start))
-
-        #print(new_f0_values)
 
         audio_info = [words_per_second, tempo]
         audio_info.extend(new_f0_values)
@@ -122,6 +127,10 @@ class Dataset():
 
     def __getitem__(self, index):
         record = self.data[index]
-        audio = self.read_audio(record["audio_path"])
-        return (audio, torch.tensor(int(record["label"])))
+        images = self.read_image(record["image_path"])
+        texts  = self.read_text(record["text_path"])
+        audios = self.read_audio(record["audio_path"])
+        labels = torch.tensor(int(record["label"]))
+        videoNames = torch.tensor(int(record["videoName"])) 
+        return (images, texts, audios, labels, videoNames)
     
